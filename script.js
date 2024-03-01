7d2f75d34cc11c980a52cedbba5e73ed2f4ac65c
Discohagen <leonziegenhagen22@gmail.com>
2024-03-01 23:44:17 +0100

    ""

--- script.js
+++ script.js
@@ -1,126 +1,57 @@
-function handleKeyDown(event) {
-    if (
-        event.target === null ||
-        event.target instanceof HTMLInputElement === false
-    ) {
-        return;
-    }
-    let focusedInput = event.target;
-    let keyDown = event.key;
-    if (/\d/.test(keyDown)) {
-        console.log('Digit down');
-        if (
-            focusedInput.id != 'input4' &&
-            focusedInput.selectionStart === 3
-        ) {
-            let increment = (
-                parseInt(focusedInput.id.match(/\d+/)[0]) +
-                1
-            ).toString();
-            document
-                .getElementById('input' + increment)
-                .focus();
+const init = () => {
+    const form = document.getElementById('form');
+
+    form.addEventListener('submit', () => {
+        const mode = document.getElementById('mode').value;
+        const input = document.getElementById('ipv4').value;
+        const output = document.getElementById('output');
+        const resultList = [];
+        let result = '';
+
+        if (isValid(input)) {
         }
-    } else {
-        switch (keyDown) {
-            /* case 'Control':
-                console.log('Control down');
-                break; */
-            case 'Backspace':
-                console.log('Backspace down');
-                if (
-                    focusedInput.id != 'input1' &&
-                    focusedInput.selectionStart === 0
-                ) {
-                    let increment = (
-                        parseInt(
-                            focusedInput.id.match(/\d+/)[0]
-                        ) - 1
-                    ).toString();
-                    document
-                        .getElementById('input' + increment)
-                        .focus();
+
+        octets = input.split('.');
+
+        switch (mode) {
+            case 'decimal -> binary':
+                for (let octetString in octets) {
+                    let binary = '';
+                    let octet = parseInt(octetString);
+                    while (octet > 0) {
+                        [octet, rest] = divMod(octet, 2);
+                        console.log(octet, rest);
+                        binary = rest + binary;
+                    }
+                    resultList.push(binary);
                 }
+                result = resultList.join('.');
                 break;
-            case 'Enter':
-                console.log('Enter down');
+            case 'decimal -> hexadecimal':
                 break;
-            case 'Tab':
-                console.log('Tab down');
+            case 'binary -> decimal':
                 break;
-            case 'ArrowRight':
-                console.log('Right Arrow down');
-                if (
-                    focusedInput.id != 'input4' &&
-                    focusedInput.selectionStart ===
-                        focusedInput.value.length
-                ) {
-                    let increment = (
-                        parseInt(
-                            focusedInput.id.match(/\d+/)[0]
-                        ) + 1
-                    ).toString();
-                    document
-                        .getElementById('input' + increment)
-                        .focus();
-                }
+            case 'binary -> hexadecimal':
                 break;
-            case 'ArrowLeft':
-                console.log('Left Arrow down');
-                if (
-                    focusedInput.id != 'input1' &&
-                    focusedInput.selectionStart === 0
-                ) {
-                    let increment = (
-                        parseInt(
-                            focusedInput.id.match(/\d+/)[0]
-                        ) - 1
-                    ).toString();
-                    document
-                        .getElementById('input' + increment)
-                        .focus();
-                }
+            case 'hexadecimal -> decimal':
+                break;
+            case 'hexadecimal -> binary':
                 break;
             default:
-                console.log('Key down does not matter');
-                event.preventDefault();
                 break;
         }
-    }
-    return;
-}
 
-function validInputForLogic(input) {}
+        output.value = result;
+        console.log(result);
+    });
+};
 
-function parseDecimalToBinary(dec) {}
+const isValid = () => {
+    return true;
+};
 
-function parseDecimalToHex(dec) {}
-
-function parseBinaryToDecimal(bin) {}
-
-function parseBinaryToHex(bin) {}
-
-function parseHexToDecimal(hex) {}
-
-function parseHexToBinary(hex) {}
-
-function handleSubmit() {}
-
-function init() {
-    // form handleSubmit
-    document
-        .querySelector('form')
-        .addEventListener('submit', handleSubmit);
-
-    // inputs validInputAtClient
-    document
-        .querySelectorAll('.ipv4_input')
-        .forEach((input) => {
-            input.addEventListener(
-                'keydown',
-                handleKeyDown
-            );
-        });
-}
+const divMod = (x, y) => {
+    return [Math.trunc(x / y), x % y];
+};
 
 document.addEventListener('DOMContentLoaded', init);
